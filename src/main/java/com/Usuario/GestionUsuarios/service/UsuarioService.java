f4610156cbde494574f881664916f798a1732521
Dylan Fernandez <dy.fernandezl@duocuc.cl>
2026-05-07 01:21:40 -0400

    Progeso de UsuarioService (En desarrollo)

--- src/main/java/com/Usuario/GestionUsuarios/service/UsuarioService.java
+++ src/main/java/com/Usuario/GestionUsuarios/service/UsuarioService.java
@@ -5,12 +5,13 @@ import com.Usuario.GestionUsuarios.dto.UsuarioResponseDTO;
 import com.Usuario.GestionUsuarios.model.Usuario;
 import com.Usuario.GestionUsuarios.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
@@ -41,9 +42,39 @@ public class UsuarioService {
     }
 
     // Agregar usuario
-    public UsuarioResponseDTO guardarUsuario(UsuarioResponseDTO dto){
-        .findBy
+    public UsuarioResponseDTO registrarUsuario(UsuarioRequestDTO dto){
+        log.info("Iniciando registros de nuevo usuario: {}",dto.getNombre());
+        if (usuarioRepository.existsByGmail(dto.getGmail())){
+            log.error("El gmail ya esta registrado");
+            throw new RuntimeException("El correo exite");
+        }
+
+        Usuario usuario = new Usuario();
+        usuario.setNombre(dto.getNombre());
+        usuario.setGmail(dto.getGmail());
+        usuario.setRol(dto.getRol());
+        usuario.setContrasena(dto.getContrasena());
+
+        usuario = usuarioRepository.save(usuario);
+        log.info("Usuario creado con exitosamente con ID: {}",usuario.getId());
+        return  mapToDTO(usuario);
     }
+
+    //auntenticar usuario por gmail
+    public Optional<UsuarioResponseDTO> obtenerPorGmail(String gmail){
+        log.info("Buscando por el correo: {} ",gmail);
+        return usuarioRepository.encontrarParaAutenticacion(gmail)
+                .map(this::mapToDTO);
+
+
+    }
+
+
+    // Actualizar por id
+
+    // eliminar por id
+
+    // Listar usuario por rol?
 
 
 
