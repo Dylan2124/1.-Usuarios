2f4533af5a0f89927343ef900cdf9a2451a52a2d
VI-Alumno <dy.fernandezl@duocuc.cl>
2026-05-15 09:58:44 -0400

    Poblacion de datos para hacer las consulta Postman

--- src/main/java/com/Usuario/GestionUsuarios/Confi/DataInitializer.java
+++ src/main/java/com/Usuario/GestionUsuarios/Confi/DataInitializer.java
@@ -22,10 +22,21 @@ public class DataInitializer implements CommandLineRunner {
         }
         log.info(">>> Cargando usuario y roles iniciales para la Plataforma de Hardware.");
 
-        repository.save(new Usuario(null,"Juan torres","admin@hardware.cl","Admin del sistema","admin123"));
-        repository.save(new Usuario(null,"Felipe contreras","felipe.ensamblaje@hardware.cl","Tecnico de ensamblaje","tec12345"));
-        repository.save(new Usuario(null,"Diego ","diego@gmail.cl","Cliente","cliente1"));
+        // ADMINISTRADORES
+        repository.save(new Usuario(null, "Juan Torres", "admin@hardware.cl", "Administrador", "adm123"));
+        repository.save(new Usuario(null, "Maria Lopez", "m.lopez@hardware.cl", "Administrador", "mira9"));
 
+        // TÉCNICOS DE ENSAMBLAJE
+        repository.save(new Usuario(null, "Felipe Contreras", "f.contreras@hardware.cl", "Técnico de Ensamblaje", "tec77"));
+        repository.save(new Usuario(null, "Carla Mendez", "c.mendez@hardware.cl", "Técnico de Ensamblaje", "fixPC"));
+        repository.save(new Usuario(null, "Andrés Silva", "a.silva@hardware.cl", "Técnico de Ensamblaje", "hardw2"));
+
+        // CLIENTES
+        repository.save(new Usuario(null, "Diego Gomez", "diego.g@gmail.com", "Cliente", "diego1"));
+        repository.save(new Usuario(null, "Ana Rojas", "ana.rojas@gmail.com", "Cliente", "ana00"));
+        repository.save(new Usuario(null, "Roberto Jara", "rjara@outlook.com", "Cliente", "rob9"));
+        repository.save(new Usuario(null, "Lucía Soto", "lucia.soto@vtr.net", "Cliente", "luci5"));
+        repository.save(new Usuario(null, "Esteban Quito", "equito@gmail.com", "Cliente", "est77"));
 
     }
 }
